cb4af4228efdbf7fcd8f5694a8c30951c8f3b305
root <root@msicentos.test.local>
2017-08-08 15:08:16 +0200

    Added shared functions for hosts file editing and updates to template.

--- gawati/installer/include.sh
+++ gawati/installer/include.sh
@@ -5,6 +5,34 @@ function set_environment_java {
   . ~/.javarc
   }
 
+
+function installer_init {
+  declare -g INSTANCE="${1}"
+  declare -g OUTFILE="${2}"
+  declare -g SRCURL="${3}"
+  declare -g INSTALLER_NAME="${INSTALLS[$INSTANCE]}"
+  declare -g INSTALLSRC="${DOWNLOADFOLDER}/${OUTFILE}"
+  vardebug INSTANCE OUTFILE SRCURL INSTALLER_NAME INSTALLSRC
+
+  declare -g RUNAS_USER="`iniget \"${INSTANCE}\" user`"
+  [ "${RUNAS_USER}" = "" ] && declare -g RUNAS_USER="${USER}"
+  vardebug RUNAS_USER
+  grep "^${RUNAS_USER}:.*" /etc/passwd >/dev/null || useradd "${RUNAS_USER}" || bail_out 1 "Failed to add missing user >${RUNAS_USER}<."
+
+  declare -g INSTANCE_FOLDER="`iniget \"${INSTANCE}\" instanceFolder`"
+  vardebug INSTANCE_FOLDER
+  declare -g INSTANCE_PATH="`echo eval echo ${INSTANCE_FOLDER} | sudo -u \"${RUNAS_USER}\" bash -s`" || bail_out 1 "Failed to determine instance folder for >${INSTANCE}<."
+  vardebug INSTANCE_PATH
+  declare -g OPTIONS="`crudini --get \"${INIFILE}\" \"${INSTANCE}\" options`"
+  vardebug OPTIONS
+
+  [ "${OUTFILE}" = "" ] || [ -f "${INSTALLSRC}" ] || {
+    download "${INSTALLSRC}" "${SRCURL}" || bail_out 2 "Failed to download >${OUTFILE}< from >${URL}<."
+    declare -g OUTFILE=""
+    }
+  }
+
+
 function cfgwrite {
   SRCFILE="${1}"
   DSTFOLDER="${2}"
@@ -32,29 +60,29 @@ function cfgwrite {
   chcon -u system_u "${DSTFILE}"
   }
 
-function installer_init {
-  declare -g INSTANCE="${1}"
-  declare -g OUTFILE="${2}"
-  declare -g SRCURL="${3}"
-  declare -g INSTALLER_NAME="${INSTALLS[$INSTANCE]}"
-  declare -g INSTALLSRC="${DOWNLOADFOLDER}/${OUTFILE}"
-  vardebug INSTANCE OUTFILE SRCURL INSTALLER_NAME INSTALLSRC
 
-  declare -g RUNAS_USER="`iniget \"${INSTANCE}\" user`"
-  [ "${RUNAS_USER}" = "" ] && declare -g RUNAS_USER="${USER}"
-  vardebug RUNAS_USER
-  grep "^${RUNAS_USER}:.*" /etc/passwd >/dev/null || useradd "${RUNAS_USER}" || bail_out 1 "Failed to add missing user >${RUNAS_USER}<."
+function cfgdeploy {
+  FILES="${1}"
+  DSTFOLDER="${2}"
+  SRCFOLDER="${3}"
 
-  declare -g INSTANCE_FOLDER="`iniget \"${INSTANCE}\" instanceFolder`"
-  vardebug INSTANCE_FOLDER
-  declare -g INSTANCE_PATH="`echo eval echo ${INSTANCE_FOLDER} | sudo -u \"${RUNAS_USER}\" bash -s`" || bail_out 1 "Failed to determine instance folder for >${INSTANCE}<."
-  vardebug INSTANCE_PATH
-  declare -g OPTIONS="`crudini --get \"${INIFILE}\" \"${INSTANCE}\" options`"
-  vardebug OPTIONS
+  [ "${SRCFOLDER}" = "" ] && SRCFOLDER+=/
 
-  [ "${OUTFILE}" = "" ] || [ -f "${INSTALLSRC}" ] || {
-    download "${INSTALLSRC}" "${SRCURL}" || bail_out 2 "Failed to download >${OUTFILE}< from >${URL}<."
-    declare -g OUTFILE=""
-    }
+  for FILE in ${FILES} ; do
+    cfgwrite "${SRCFOLDER}${FILE}" "${DSTFOLDER}"
+    done
+  }
+
+
+function addtohosts {
+  IP="${1}"
+  NAMES="${*:2}"
+  echo "$(cat /etc/hosts)" | grep -v "^${IP}\s" >/tmp/hosts.tmp
+  HOSTSdata="`grep \"^${IP}\s\" /etc/hosts`"
+  HOSTSdata+=" ${NAMES}"
+  HOSTSdata="`echo ${HOSTSdata} |  tr ' ' '\n' | tail -n +2 | sort -ur | xargs echo -n`"
+  echo "${IP}       ${HOSTSdata}" >>/tmp/hosts.tmp
+  cat /tmp/hosts.tmp >/etc/hosts
+  rm -f /tmp/hosts.tmp
   }
 
--- gawati/installer/template/scripts/template.sh
+++ gawati/installer/template/scripts/template.sh
@@ -2,9 +2,6 @@ function install {
   VERSION="${2}"
   installer_init "${1}" "examplefile.png" "http://africaninnovation.org/img/logo.png"
 
-  # Make silently sure, that bash is installed
-  OSinstall bash 1
-
   message 1 "This is a demo / template for writing installers, listing available predefined variables. It will not make any changes."
   message 1 "This template was invoked with instance name >${INSTANCE}<. This name might be used as a service name for starting at boot time."
   message 1 "This installer is called >${INSTALLER_NAME}<."
@@ -13,6 +10,15 @@ function install {
   message 1 "The package is intended to be installed for or as user >${RUNAS_USER}< and if started on bootup, should run as that user. If missing, the user was created by installer_init()."
   message 1 "The package is to be installed into folder >${INSTANCE_PATH}<."
   message 1 "If custom options were specified, these are the ones: >${OPTIONS}<"
-  message 1 "Additional custom configuration items can be fetched from the ini file by calling >iniget \"${INSTANCE}\" myitem<."
+
+  # Fetch a custom configiration item from ini file
+  #MYITEM="`iniget \"${INSTANCE}\" myitem`"
+
+  # Make silently sure, that bash is installed
+  #OSinstall bash 1
+
+  # Write a config file from source template applying expansion on contained shell environment variables, optionally using new destination filename
+  #cfgwrite "${SRCFOLDER}/${SRC_FILENAME}" "${DSTFOLDER}" "${OPTIONAL_DSTFILENAME}"
+
   }
 
