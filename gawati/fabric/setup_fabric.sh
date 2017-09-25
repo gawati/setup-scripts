2bb0c2b6a271fc9b063564398ef7141ab054e152
kohsah <ashok@hariharan.org.in>
2017-09-25 14:13:27 +0530

    Updates to fabric setup; Added python-virtualenv and python-pip to essentials :

--- gawati/fabric/setup_fabric.sh
+++ gawati/fabric/setup_fabric.sh
@@ -8,17 +8,17 @@
 FABVENV=venv_fabric
 
 # python pip and fab relative paths
-FABPY=./$FABVENV/bin/python
-FABPIP=./$FABVENV/bin/pip
-FAB=./$FABVENV/bin/fab
-FABSRC=./fabric
+FABPY=../$FABVENV/bin/python
+FABPIP=../$FABVENV/bin/pip
+FAB=../$FABVENV/bin/fab
+FABSRC=.
 
 # install pip
-curl -z  -"get-pip.py"  "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py && rm -rf get-pip.py
+#curl -z  -"get-pip.py"  "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+#python get-pip.py && rm -rf get-pip.py
 
 # install virtualenv
-pip install virtualenv
+#pip install virtualenv
 
 # install the fabric virtual python
 virtualenv $FABVENV
@@ -26,5 +26,5 @@ virtualenv $FABVENV
 # install fabric in the virtual env, we dont
 # want to mess with the system python
 $FABPIP install fabric
-echo "cd $FABSRC && .$FAB \$*" > ./fab && chmod ug+x ./fab
-echo "./fab -H localhost \$*" > ./fl && chmod ug+x ./fl
+echo "cd $FABSRC && $FAB \$*" > ../fab && chmod ug+x ../fab
+echo "./fab -H localhost \$*" > ../fl && chmod ug+x ../fl
