b4209b84ab0320e859e5599fe921b74e42f7414c
ccsmart <root@msicentos.test.local>
2017-10-10 12:18:27 +0200

    Deploy templates

--- gawati/installer/gawatiportal/scripts/gawatiportal01.sh
+++ gawati/installer/gawatiportal/scripts/gawatiportal01.sh
@@ -2,7 +2,9 @@
 
 function install {
   VERSION="${2}"
-  installer_init "${1}" "" ""
+  ZIPFILE="gawati-templates-${VERSION}.zip"
+
+  installer_init "${1}" "${ZIPFILE}" "https://github.com/gawati/gawati-templates/releases/download/${VERSION}/${ZIPFILE}"
   
   CFGSRC="${INSTALLER_HOME}/01"
   CFGDST="/etc/httpd/conf.d"
@@ -15,10 +17,16 @@ function install {
 
   cfgwrite "${CFGSRC}/10-gawati.conf" "${CFGDST}" "10-${GAWATI_URL_ROOT}.conf"
 
-  DSTOBJ="/var/www/html/${GAWATI_URL_ROOT}"
-  [ -e "${DSTOBJ}" ] || {
-    mkdir -p "${DSTOBJ}"
-    chown root:apache "${DSTOBJ}"
+  WWWROOT="/var/www/html/${GAWATI_URL_ROOT}"
+  [ -e "${WWWROOT}" ] || {
+    mkdir -p "${WWWROOT}"
+    chown root:apache "${WWWROOT}"
+    }
+
+  GWTEMPLATES="${WWWROOT}/gwtemplates"
+  [ -e "${GWTEMPLATES}" ] || {
+    mkdir -p "${GWTEMPLATES}"
+    chown root:apache "${GWTEMPLATES}"
     }
 
   DSTOBJ="/etc/httpd/logs/${GAWATI_URL_ROOT}"
@@ -28,30 +36,22 @@ function install {
     chmod 770 "${DSTOBJ}"
     }
 
+  OSinstall unzip
+
+  [ -f "${DOWNLOADFOLDER}/${ZIPFILE}" ] || bail_out "Template package not available at >${DOWNLOADFOLDER}/${ZIPFILE}<"
+  unzip "${DOWNLOADFOLDER}/${ZIPFILE}" -d "${GWTEMPLATES}"
+  [ -d "${GWTEMPLATES}/themes" ] || bail_out "Failed to deploy themes."
+  chown -R root:apache "${GWTEMPLATES}/themes"
+
+
+  #--------------------
+
   false && {
-  OSinstall fabric
-
-  pushd "${DOWNLOADFOLDER}" >/dev/null
-  [ -d "gawati" ] &&  {
-    cd gawati
-    svn update >/dev/null
-    message 1 "Updated gawati installers..."
-    } ||  {
-    message 1 "Fetching gawati installers..."
-    svn checkout "https://github.com/gawati/setup-scripts.git/trunk/gawati/fabric" gawati >/dev/null
-    }
-  popd >/dev/null
-
   XSTBE="`iniget \"${INSTANCE}\" existbe`"
   XSTST="`iniget \"${INSTANCE}\" existst`"
 
   BEPWD="`getvar adminPasswd ${XSTBE}`"
   STPWD="`getvar adminPasswd ${XSTST}`"
-
-  BUILDUSER="`iniget \"${INSTANCE}\" builduser`"
-  export BEPWD STPWD BUILDUSER
-
-  ensureuser "${BUILDUSER}"
 
   [ -e ~/.ssh/id_rsa.pub ] || {
     [ -e ~/.ssh/id_rsa ] || {
@@ -75,19 +75,6 @@ function install {
 
   grep "${MYKEY}" ~/.ssh/authorized_keys >/dev/null || echo "${MYKEY}" >>~/.ssh/authorized_keys
 EndOfScriptAsRUNAS_USER
-
-
-  [ "${BEPWD}" = "" ] && {
-    echo "Please provide the administrator password for eXist instance >${XSTBE}<."
-    read BEPWD
-    }
-
-  [ "${STPWD}" = "" ] && {
-    echo "Please provide the administrator password for eXist instance >${XSTST}<."
-    read STPWD
-    }
-
-  cat "${CFGSRC}/deployment.ini" | envsubst | "${DOWNLOADFOLDER}/gawati/setup_fabric.sh"
   }
 
   }
