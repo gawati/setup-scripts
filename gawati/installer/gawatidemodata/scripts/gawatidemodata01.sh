2a6e031c9c8e05de9372a7378c5d6bf2a6da48f0
ccsmart <root@msicentos.test.local>
2017-10-19 10:21:17 +0200

    Create separate demodata module

---
+++ gawati/installer/gawatidemodata/scripts/gawatidemodata01.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function install {
+  VERSION="${2}"
+
+  installer_init "${1}" "" ""
+
+  PDFZIP="${DOWNLOADFOLDER}/akn_pdf_sample-${VERSION}.zip"
+  XMLZIP="${DOWNLOADFOLDER}/akn_xml_sample-${VERSION}.zip"
+  vardebug PDFZIP XMLZIP
+
+  download "${PDFZIP}" "https://github.com/gawati/gawati-data-xml/releases/download/${VERSION}/akn_pdf_sample-${VERSION}.zip"
+  download "${XMLZIP}" "https://github.com/gawati/gawati-data-xml/releases/download/${VERSION}/akn_xml_sample-${VERSION}.zip"
+
+  OSinstall unzip 1
+
+  GAWATI_URL_ROOT="`getvar GAWATI_URL_ROOT gawatiproxy`"
+  askifempty GAWATI_URL_ROOT "Please provide the full public DNS hostname for your Gawati server."
+  WWWROOT="/var/www/html/${GAWATI_URL_ROOT}"
+  vardebug GAWATI_URL_ROOT WWWROOT
+
+  [ -f "${PDFZIP}" ] || bail_out "Demodata documents package not available at >${PDFZIP}<"
+  unzip -q "${PDFZIP}" -d "${WWWROOT}"
+  [ -d "${WWWROOT}/akn" ] || bail_out "Failed to deploy documents."
+  chown -R root:apache "${WWWROOT}/akn"
+
+  STIMPORT="`iniget \"${INSTANCE}\" importFolder`"
+  [ -e "${STIMPORT}" ] || mkdir -p "${STIMPORT}"
+
+  [ -f "${XMLZIP}" ] || bail_out "XML demodata package not available at >${XMLZIP}<"
+  unzip -q "${XMLZIP}" -d "${STIMPORT}"
+  [ -d "${STIMPORT}/akn" ] || bail_out "Failed to deploy documents."
+  chown -R root:apache "${STIMPORT}/akn"
+
+  [ -d "${STIMPORT}" ] && {
+    XSTST="`iniget \"${INSTANCE}\" existst`"
+
+    STUSER="`getvar RUNAS_USER ${XSTST}`"
+    STHOME="`getvar EXIST_HOME ${XSTST}`"
+    STPORT="`getvar EXIST_PORT ${XSTST}`"
+    STPWD="`getvar adminPasswd ${XSTST}`"
+
+    askifempty STPWD "Please provide the administrator password for eXist instance >${XSTST}<."
+    setvar adminPasswd "${STPWD}" "${XSTST}"
+
+    STDATAPWD="`${STHOME}/bin/client.sh -ouri=xmldb:exist://localhost:${STPORT}/exist/xmlrpc -u admin -P """${STPWD}""" -x """data(doc('/db/apps/gw-data/_auth/_pw.xml')/users/user[@name = 'gwdata']/@pw)""" 2>/dev/null | tail -1`"
+
+    vardebug XSTST STIMPORT STUSER STHOME STPORT STPWD STDATAPWD
+
+    message 1 "Importing Data into exist instance >${XSTST}<. This can take a while."
+    RESULT="`${STHOME}/bin/client.sh -ouri=xmldb:exist://localhost:${STPORT}/exist/xmlrpc -u gwdata -P """${STDATAPWD}""" -d -m /db/apps/gw-data/akn -p """${STIMPORT}/akn""" 2>/dev/null | tail -1`"
+    message 1 "${RESULT}"
+    }
+  }
+
--- gawati/installer/gawatiportal/scripts/gawatiportal01.sh
+++ gawati/installer/gawatiportal/scripts/gawatiportal01.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-function install {
+function readconfig {
   VERSION="${2}"
   ZIPFILE="gawati-templates-${VERSION}.zip"
 
   installer_init "${1}" "${ZIPFILE}" "https://github.com/gawati/gawati-templates/releases/download/${VERSION}/${ZIPFILE}"
   
-  CFGSRC="${INSTALLER_HOME}/01"
-  CFGDST="/etc/httpd/conf.d"
-
   export GAWATI_URL_ROOT="`iniget \"${INSTANCE}\" GAWATI_URL_ROOT`"
   export GAWATI_URL_ROOT_="`echo ${GAWATI_URL_ROOT} | tr . _`"
   export EXIST_ST_URL="`iniget \"${INSTANCE}\" EXIST_ST_URL`"
+
+  setvars GAWATI_URL_ROOT GAWATI_URL_ROOT_ EXIST_ST_URL
+  }
+
+function install {
+  CFGSRC="${INSTALLER_HOME}/01"
+  CFGDST="/etc/httpd/conf.d"
 
   addtohosts "${MainIP}" "${GAWATI_URL_ROOT}"
 
@@ -36,33 +40,21 @@ function install {
     chmod 770 "${DSTOBJ}"
     }
 
-  OSinstall unzip
+  OSinstall unzip 1
 
   [ -f "${DOWNLOADFOLDER}/${ZIPFILE}" ] || bail_out "Template package not available at >${DOWNLOADFOLDER}/${ZIPFILE}<"
   unzip -q "${DOWNLOADFOLDER}/${ZIPFILE}" -d "${GWTEMPLATES}"
   [ -d "${GWTEMPLATES}/themes" ] || bail_out "Failed to deploy themes."
   chown -R root:apache "${GWTEMPLATES}/themes"
 
-  STIMPORT="`iniget \"${INSTANCE}\" importFolder`"
+  systemctl restart httpd
 
-  [ -d "${STIMPORT}" ] && {
-    XSTST="`iniget \"${INSTANCE}\" existst`"
-
-    STUSER="`getvar RUNAS_USER ${XSTST}`"
-    STHOME="`getvar EXIST_HOME ${XSTST}`"
-    STPORT="`getvar EXIST_PORT ${XSTST}`"
-    STPWD="`getvar adminPasswd ${XSTST}`"
-
-    askifempty STPWD "Please provide the administrator password for eXist instance >${XSTST}<."
-    setvar adminPasswd "${STPWD}" "${XSTST}"
-
-    STDATAPWD="`${STHOME}/bin/client.sh -ouri=xmldb:exist://localhost:${STPORT}/exist/xmlrpc -u admin -P """${STPWD}""" -x """data(doc('/db/apps/gw-data/_auth/_pw.xml')/users/user[@name = 'gwdata']/@pw)""" 2>/dev/null | tail -1`"
-
-    vardebug XSTST STIMPORT STUSER STHOME STPORT STPWD STDATAPWD
-
-    message 1 "Importing Data into exist instance >${XSTST}<. This can take a while."
-    RESULT="`${STHOME}/bin/client.sh -ouri=xmldb:exist://localhost:${STPORT}/exist/xmlrpc -u gwdata -P """${STDATAPWD}""" -d -m /db/apps/gw-data/akn -p """${STIMPORT}""" 2>/dev/null | tail -1`"
-    message 1 "${RESULT}"
+  FILE="/usr/local/bin/fixthumbs"
+  [ -e "${FILE}" ] || {
+    cat "${CFGSRC}/fixthumbs" >"${FILE}"
+    chcon -u system_u "${FILE}"
+    chmod 755 "${FILE}"
     }
+
   }
 
