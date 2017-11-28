7fc16dce70858a2b10eaa246e54e41fc02947124
ccsmart <michael@gawati.org>
2017-11-28 14:03:45 +0100

    Rename admin preinstaller

--- gawati/citools/gawati_preinstall_admin.cmd
+++ gawati/citools/gawati_preinstall_admin.cmd
@@ -1,4 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
 choco install kitty 7zip virtualbox -y
 
 setlocal
