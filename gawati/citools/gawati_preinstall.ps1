640d403e1f505fb5f8c7bec932047ec41481d065
ccsmart <michael@gawati.org>
2017-11-28 16:07:00 +0100

    Leave admin invocation to top installer

--- gawati/citools/gawati_preinstall.ps1
+++ gawati/citools/gawati_preinstall.ps1
@@ -1,7 +1,5 @@
 $GawatiOSURL = "https://gawati.org/GawatiVM.7z"
 
-Start-Process "$psHome\powershell.exe" -verb runas -ArgumentList "-noprofile -file '$PSScriptRoot\gawati_preinstall_admin.ps1'"
-
 $ZIPFile = "$Env:USERPROFILE\Downloads\GawatiVM.7z"
 (New-Object System.Net.WebClient).DownloadFile($GawatiOSURL,$ZIPFile)
 
