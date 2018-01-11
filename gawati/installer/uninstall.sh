35e5c4b92fe8a961d4353f33e6241a139c5f3352
ccsmart <michael@gawati.org>
2018-01-11 09:32:44 +0100

    Align html directory with dev.ini

--- gawati/installer/uninstall.sh
+++ gawati/installer/uninstall.sh
@@ -8,8 +8,7 @@ yes | rm /etc/init.d/jetty-dev01
 yes | rm -rf /home/xstbe/apps
 yes | rm -rf /home/xstst/apps
 yes | rm -rf /home/dev01/apps
-yes | rm -rf /var/www/html/alldev.gawati.org
-yes | rm -rf /var/www/html/my.gawati.org
+yes | rm -rf /var/www/html/my.gawati.local
 yes | rm -rf /root/import/*
 yes | rm -rf /opt/Download/gawati-templates-*
 yes | rm -rf /opt/Download/akn_*_sample-*
