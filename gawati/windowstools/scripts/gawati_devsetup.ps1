c3dcbc8e9c4602872c4cbb113d4e8717878b1160
ccsmart <michael@gawati.org>
2018-01-16 16:53:53 +0100

    Powershell and SSH... :/

--- gawati/windowstools/scripts/gawati_devsetup.ps1
+++ gawati/windowstools/scripts/gawati_devsetup.ps1
@@ -26,27 +26,34 @@ VBoxManage snapshot $BOXNAME take $SNAPNAME
 VBoxManage clonevm $BOXNAME --snapshot $SNAPNAME --options link --options keepallmacs --name $TESTNAME --register
 VBoxManage startvm $TESTNAME --type headless
 
-WaitForVM
-
+Import-Module Posh-SSH
+#Get-Command -Module Posh-SSH
 
 #kitty -pw MyGawatiLocal -ssh root@my.gawati.local -m scripts\gawati_devsetup.sh -log $LOG
 #cmd /c start powershell -Command "& { Get-Content -Path $LOG -Wait }"
 
-Get-Command -Module Posh-SSH
+WaitForVM
 
 $GAWATIHOST="my.gawati.local"
 $SSHusr = "root"
 $SSHpwd = ConvertTo-SecureString "MyGawatiLocal" -AsPlainText -Force
 $SSHcred = New-Object System.Management.Automation.PSCredential ($SSHusr, $SSHpwd)
 
-$SSHsession = New-SSHSession -ComputerName $GAWATIHOST -Credential $SSHcred -Force
-
 $SSHlog = ""
 
-Get-Content $InstallScript | ForEach-Object {
-  $SSHresult = Invoke-SSHCommand -SSHSession $SSHsession -Timeout 600 -Command $_
-  $SSHlog += $SSHresult.Output | Out-String
-  }
+$SSHsession = New-SSHSession -ComputerName $GAWATIHOST -Credential $SSHcred -Force
+
+Set-SCPFile -ComputerName $GAWATIHOST -Credential $SSHcred -LocalFile "gawati_devsetup.sh" -RemotePath "/root/" -Force
+
+$SSHresult = Invoke-SSHCommand -SSHSession $SSHsession -Timeout 10 -Command 'touch /var/log/setup.log'
+kitty -pw MyGawatiLocal -ssh root@my.gawati.local -log $LOG -cmd 'tail -f /var/log/setup.log'
+
+$SSHresult = Invoke-SSHCommand -SSHSession $SSHsession -Timeout 10 -Command 'chmod 755 gawati_devsetup.sh'
+
+$SSHresult = Invoke-SSHCommand -SSHSession $SSHsession -Timeout 600 -Command './gawati_devsetup.sh >/var/log/setup.log 2>&1'
+
+$SSHresult = Invoke-SSHCommand -SSHSession $SSHsession -Timeout 10 -Command 'cat /var/log/setup.log'
+$SSHlog = $SSHresult.Output | Out-String
 
 Remove-SSHSession -SSHSession $SSHsession -Verbose
 
