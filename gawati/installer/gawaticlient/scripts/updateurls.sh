bca9b2a7924eca418670fbd090b85038ca7de7a7
ccsmart <michael@gawati.org>
2018-04-12 14:49:55 +0200

    reference data service in PROXY

--- gawati/installer/gawaticlient/scripts/updateurls.sh
+++ gawati/installer/gawaticlient/scripts/updateurls.sh
@@ -2,7 +2,7 @@ function postinstall {
   INDEXHTML="${EDITROOT}/index.html"
   vardebug INDEXHTML
   [ -f "${INDEXHTML}" ] || bail_out "File >${INDEXHTML}< missing."
-  sed -i'' "s%GAWATI_PROXY:\"[^\"]*\"%GAWATI_PROXY:\"https://${GAWATI_URL_ROOT}\"%g" "${INDEXHTML}"
+  sed -i'' "s%GAWATI_PROXY:\"[^\"]*\"%GAWATI_PROXY:\"https://data.${GAWATI_URL_ROOT}\"%g" "${INDEXHTML}"
   sed -i'' "s%GAWATI_DOCUMENT_SERVER:\"[^\"]*\"%GAWATI_DOCUMENT_SERVER:\"https://media.${GAWATI_URL_ROOT}\"%g" "${INDEXHTML}"
   }
 
