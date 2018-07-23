4918ff84d9965ecb841a6f720747e3f699f0ca6d
ccsmart <michael@gawati.org>
2018-07-23 10:07:17 +0200

    Remove obsolete entries (now delivered by gawati-portal-fe)

--- gawati/installer/gawatiportal/scripts/updateurls.sh
+++ gawati/installer/gawatiportal/scripts/updateurls.sh
@@ -3,7 +3,5 @@ function postinstall {
   vardebug INDEXHTML
   [ -f "${INDEXHTML}" ] || bail_out "File >${INDEXHTML}< missing."
   sed -i'' "s%GAWATI_PROXY:\"[^\"]*\"%GAWATI_PROXY:\"https://data.${GAWATI_URL_ROOT}\"%g" "${INDEXHTML}"
-  sed -i'' "s%GAWATI_DOCUMENT_SERVER:\"[^\"]*\"%GAWATI_DOCUMENT_SERVER:\"https://media.${GAWATI_URL_ROOT}\"%g" "${INDEXHTML}"
-  sed -i'' "s%GAWATI_PROFILE_SERVER:\"[^\"]*\"%GAWATI_PROFILE_SERVER:\"https://${GAWATI_URL_ROOT}/profiles\"%g" "${INDEXHTML}"
   }
 
